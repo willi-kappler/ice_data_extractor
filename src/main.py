3ff28ecef1b9782db2d394070862a851bc2c7d60
Willi Kappler <willi.kappler.gm@gmail.com>
2024-10-10 22:29:59 +0200

    Add plotting of original and extracted data.

--- src/main.py
+++ src/main.py
@@ -6,12 +6,37 @@ import sys
 import time
 
 # External imports
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Local imports
 import extractor
 
 logger = logging.getLogger(__name__)
 
+
+# Call this Python script with:
+# python3 src/main.py -1.23 -500.0 assets/Rutford_DELORES_TWTT_2D_201617_500m_spaced
+
+def plot_data(ex: extractor.Extractor):
+    x1 = np.array(ex.original_points_x)
+    y1 = np.array(ex.original_points_y)
+    z1 = np.array(ex.original_points_z)
+
+    x2 = np.array(ex.extracted_points_x)
+    y2 = np.array(ex.extracted_points_y)
+    z2 = np.array(ex.extracted_points_z)
+
+    fig, (ax1, ax2) = plt.subplots(2)
+    fig.suptitle("Heatmaps of measured points")
+
+    cmap = ax1.tricontourf(x1, y1, z1, cmap="RdBu_r")
+    fig.colorbar(cmap, ax=ax1)
+
+    ax2.tricontourf(x2, y2, z2, cmap="RdBu_r")
+    fig.colorbar(cmap, ax=ax2)
+
+    plt.savefig("plot.png")
 
 def main():
     start = time.time()
@@ -26,6 +51,8 @@ def main():
 
     ex = extractor.Extractor(angle, step_x)
     ex.read_file(filename)
+
+    plot_data(ex)
 
     end = time.time()
     duration = end - start
