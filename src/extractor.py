0fa559790dd69a11d6c856177ff467b028279b68
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-10-11 14:39:25 +0200

    Remove tiles, use interpolation from scipy module.

--- src/extractor.py
+++ src/extractor.py
@@ -3,9 +3,10 @@
 import sys
 import logging
 import math
-from operator import itemgetter
 
 # External imports:
+from scipy.interpolate import NearestNDInterpolator
+# from scipy.interpolate import CloughTocher2DInterpolator
 
 
 logger = logging.getLogger(__name__)
@@ -13,85 +14,18 @@ logger = logging.getLogger(__name__)
 PointList = list[tuple[(float, float, float)]]
 
 
-def calculate_z_value(x: float, y: float,
-        points: PointList,
-        num_of_values: int = 4) -> float:
-
-    assert num_of_values > 3, "To interpolate the z values at least 4 points must be used!"
-
-    if len(points) < num_of_values:
-        # Too few points to interpolate!
-        return 0.0
-
-    values: list[tuple[(float, float)]] = []
-    max_dist: float = sys.float_info.max
-
-    for _ in range(num_of_values):
-        values.append((max_dist, 0.0))
-
-    for (x2, y2, z) in points:
-        d: float = math.hypot(x - x2, y - y2)
-
-        if d < values[-1][0]:
-            values[-1] = (d, z)
-            values.sort(key=itemgetter(0))
-
-    exp_factor = 1.0
-
-    while True:
-        result: float = 0.0
-        factor_sum: float = 0.0
-
-        for (dist, z_val) in values:
-            factor = math.exp(-dist * exp_factor)
-            result = result + (z_val * factor)
-            factor_sum = factor_sum + factor
-
-        if factor_sum > 0.0:
-            result = result / factor_sum
-            break
-        else:
-            # logger.debug(f"Factor sum is zero! Exp factor: {exp_factor}")
-            # logger.debug(f"Values: {values}")
-            exp_factor = exp_factor * 0.5
-
-    return result
-
-
-class Tile:
-    def __init__(self, min_x: float, min_y: float,
-            max_x: float, max_y: float):
-        self.min_x: float = min_x
-        self.min_y: float = min_y
-        self.max_x: float = max_x
-        self.max_y: float = max_y
-        self.center_x: float = (self.min_x + self.max_x) / 2.0
-        self.center_y: float = (self.min_y + self.max_y) / 2.0
-        self.points: PointList = []
-
-    def point_in_tile(self, x: float, y: float) -> bool:
-        in_x: bool = (x >= self.min_x) and (x < self.max_x)
-        in_y: bool = (y >= self.min_y) and (y < self.max_y)
-
-        return in_x and in_y
-
-    def add_point(self, x: float, y: float, z: float):
-        self.points.append((x, y, z))
-
-    def calculate_z_value(self, x: float, y: float) -> float:
-        return calculate_z_value(x, y, self.points)
-
-
 class Extractor:
-    def __init__(self, angle: float, step_x: float):
-        self.angle: float = angle
-        self.step_x: float = step_x
-        self.step_y: float = 0.0
+    def __init__(self):
+        self.start_x: float = -1283169.718723
+        self.start_y: float = 124083.532967
 
-        self.tile_x: int = 100
-        self.tile_y: int = 100
-        self.num_of_tiles: int = self.tile_x * self.tile_y
-        self.tiles: list[Tile] = []
+        self.dx1: float = -6.643222999991849
+        self.dy1: float = 18.864452999987407
+        self.dx2: float = 471.61132899997756
+        self.dy2: float = 166.08056600000418
+
+        self.num_of_rows: int = 36
+        self.num_of_cols: int = 895
 
         self.start_points: PointList = []
         self.end_points: PointList = []
@@ -116,64 +50,35 @@ class Extractor:
         self.len_y: float = 0.0
         self.len_z: float = 0.0
 
-        self.tile_dx: float = 0.0
-        self.tile_dy: float = 0.0
+        self.data_dx1: float
+        self.data_dx2: float
+        self.column_length: float
+        self.data_dy1: float
+        self.data_dy2: float
+        self.row_length: float
 
-        self.last_col: int = 0
-        self.last_row: int = 0
-        self.calculated_angle: float = 0.0
-
-        self.original_dx: float = 0.0
-        self.original_dy: float = 0.0
-
-        self.row_dx: float = 0.0
-        self.row_dy: float = 0.0
+        self.interpolator: NearestNDInterpolator = NearestNDInterpolator([(0.0, 0.0)], [0.0])
 
     def read_file(self, filename: str):
         logger.info(f"Reading file: {filename}")
 
         with open(filename, "r") as f:
             self.find_min_max_values(f)
-            self.create_tiles()
             f.seek(0)  # rewind file
             self.read_data_points(f)
 
         self.extract_points()
         self.save_extracted_points()
 
-    def create_tiles(self):
-        x1: float = self.min_x
-        x2: float = x1 + self.tile_dx
-        y1: float = self.min_y
-        y2: float = y1 + self.tile_dy
-
-        for _ in range(self.num_of_tiles):
-            new_tile = Tile(x1, y1, x2, y2)
-            self.tiles.append(new_tile)
-
-            x1 += self.tile_dx
-            x2 = x1 + self.tile_dx
-
-            if x1 >= self.max_x:
-                x1 = self.min_x
-                x2 = x1 + self.tile_dx
-                y1 += self.tile_dy
-                y2 = y1 + self.tile_dy
-
-    def yield_points(self):
-        for tile in self.tiles:
-            for point in tile.points:
-                yield point
-
     def yield_values_from_file(self, file):
         for line in file:
             if line.startswith("# Grid_size:"):
                 grid_info = line[12:].split("x")
-                self.last_col = int(grid_info[0])
-                self.last_row = int(grid_info[1])
+                self.num_of_cols = int(grid_info[0])
+                self.num_of_rows = int(grid_info[1])
 
-                logger.debug(f"Last col: {self.last_col}")
-                logger.debug(f"Last row: {self.last_row}")
+                logger.debug(f"Last col: {self.num_of_cols}")
+                logger.debug(f"Last row: {self.num_of_rows}")
 
                 continue
             elif line.startswith("#"):
@@ -185,11 +90,12 @@ class Extractor:
             y: float = float(items[1])
             z: float = float(items[2])
             column: int = int(items[3])
+            row: int = int(items[4])
 
-            yield (x, y, z, column)
+            yield (x, y, z, column, row)
 
     def find_min_max_values(self, file):
-        for (x, y, z, _) in self.yield_values_from_file(file):
+        for (x, y, z, _, _) in self.yield_values_from_file(file):
             if x < self.min_x:
                 self.min_x = x
             elif x > self.max_x:
@@ -209,82 +115,64 @@ class Extractor:
         self.len_y = self.max_y - self.min_y
         self.len_z = self.max_z - self.min_z
 
-        self.tile_dx = self.len_x / float(self.tile_x)
-        self.tile_dy = self.len_y / float(self.tile_y)
-
         logger.debug(f"Min values: x: {self.min_x}, y: {self.min_y}, z: {self.min_z}")
         logger.debug(f"Max values: x: {self.max_x}, y: {self.max_y}, z: {self.max_z}")
         logger.debug(f"Length values: x: {self.len_x}, y: {self.len_y}, z: {self.len_z}")
-        logger.debug(f"Tile spacing: x: {self.tile_dx}, y: {self.tile_dy}")
 
     def read_data_points(self, file):
-        for (x, y, z, column) in self.yield_values_from_file(file):
+        for (x, y, z, column, row) in self.yield_values_from_file(file):
             if column == 1:
                 self.start_points.append((x, y, z))
-            elif column == 2:
-                if self.original_dx == 0.0:
-                    self.original_dx = x - self.start_points[0][0]
-                    self.original_dy = y - self.start_points[0][1]
-                    logger.debug(f"Steps from points: dx: {self.original_dx}, dy: {self.original_dy}")
-            elif column == self.last_col:
+            elif (column == 2) and (row == 1):
+                self.data_dx1 = x - self.start_points[0][0]
+                self.data_dy1 = y - self.start_points[0][1]
+                logger.debug(f"Column step: dx: {self.data_dx1}, dy: {self.data_dy1}")
+            elif column == self.num_of_cols:
                 self.end_points.append((x, y, z))
-
-            for tile in self.tiles:
-                if tile.point_in_tile(x, y):
-                    tile.add_point(x, y, z)
 
             self.original_points_x.append(x)
             self.original_points_y.append(y)
             self.original_points_z.append(z)
 
+        self.data_dx2 = self.start_points[1][0] - self.start_points[0][0]
+        self.data_dy2 = self.start_points[1][1] - self.start_points[0][1]
+        logger.debug(f"Row step: dx: {self.data_dx2}, dy: {self.data_dy2}")
+
         logger.debug(f"Number of starting points: {len(self.start_points)}")
 
         self.row_dx = self.end_points[0][0] - self.start_points[0][0]
         self.row_dy = self.end_points[0][1] - self.start_points[0][1]
-        logger.debug(f"Row dx: {self.row_dx}")
-        logger.debug(f"Row dy: {self.row_dy}")
+        self.row_length = math.hypot(self.row_dx, self.row_dy)
+        logger.debug(f"Total row dx: {self.row_dx}, dy: {self.row_dy}")
+        logger.debug(f"Total row length: {self.row_length}")
+
+        self.column_dx = self.start_points[-1][0] - self.start_points[0][0]
+        self.column_dy = self.start_points[-1][1] - self.start_points[0][1]
+        self.column_length = math.hypot(self.column_dx, self.column_dy)
+        logger.debug(f"Total column dx: {self.column_dx}, dy: {self.column_dy}")
+        logger.debug(f"Total column length: {self.column_length}")
+
+        self.interpolator = NearestNDInterpolator(
+            list(zip(self.original_points_x, self.original_points_y)), self.original_points_z)
 
         self.calculated_angle = math.degrees(math.atan(self.row_dy / self.row_dx))
-        logger.debug(f"Calculated angle: {self.calculated_angle}")
-
-        self.step_y = math.tan(math.radians(self.angle)) * self.step_x
-
-        logger.debug(f"X step: {self.step_x} m, Y step: {self.step_y} m")
-
-    def calculate_z_value(self, x: float, y: float) -> float:
-        for tile in self.tiles:
-            if tile.point_in_tile(x, y):
-                return tile.calculate_z_value(x, y)
-
-        return 0.0
+        logger.debug(f"Calculated row angle: {self.calculated_angle}")
 
     def extract_points(self):
-        for (sx, sy, sz) in self.start_points:
-            self.extracted_points_x.append(sx)
-            self.extracted_points_y.append(sy)
-            self.extracted_points_z.append(sz)
-            # logger.debug(f"Starting point: {sx}, {sy}, {sz}")
+        x: float = self.start_x
+        y: float = self.start_y
 
-            x: float = sx + self.step_x
-            y: float = sy + self.step_y
-
-            while True:
-                z = self.calculate_z_value(x, y)
+        for i in range(self.num_of_rows):
+            x = self.start_x + (float(i) * self.dx2)
+            y = self.start_y + (float(i) * self.dy2)
+            for _ in range(self.num_of_cols):
                 self.extracted_points_x.append(x)
                 self.extracted_points_y.append(y)
-                self.extracted_points_z.append(z)
-                # logger.debug(f"Calculated point: {x}, {y}, {z}")
+                x = x + self.dx1
+                y = y + self.dy1
 
-                x = x + self.step_x
-                y = y + self.step_y
-
-                if (x > self.max_x) or (x < self.min_x):
-                    break
-
-                if (y > self.max_y) or (y < self.min_y):
-                    break
-
-            # logger.debug(f"----------")
+        z_values = self.interpolator(self.extracted_points_x, self.extracted_points_y)
+        self.extracted_points_z = list(z_values)
 
     def save_extracted_points(self):
         with open("extracted_points.csv", "w") as f:
