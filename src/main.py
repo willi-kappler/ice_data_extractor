13678198c2727b7856abec98794f15987e0acba2
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-10-11 10:10:12 +0200

    Add start and end points to plot 1.

--- src/main.py
+++ src/main.py
@@ -19,18 +19,25 @@ logger = logging.getLogger(__name__)
 # python3 src/main.py -1.23 -500.0 assets/Rutford_DELORES_TWTT_2D_201617_500m_spaced
 
 def plot_data(ex: extractor.Extractor):
-    x1 = np.array(ex.original_points_x)
-    y1 = np.array(ex.original_points_y)
-    z1 = np.array(ex.original_points_z)
+    x1 = ex.original_points_x
+    y1 = ex.original_points_y
+    z1 = ex.original_points_z
 
-    x2 = np.array(ex.extracted_points_x)
-    y2 = np.array(ex.extracted_points_y)
-    z2 = np.array(ex.extracted_points_z)
+    x2 = ex.extracted_points_x
+    y2 = ex.extracted_points_y
+    z2 = ex.extracted_points_z
 
     fig, (ax1, ax2) = plt.subplots(2)
     fig.suptitle("Heatmaps of measured points")
 
     cmap = ax1.tricontourf(x1, y1, z1, cmap="RdBu_r")
+
+    for (x, y, _) in ex.start_points:
+        ax1.plot(x, y, "yo")
+
+    for (x, y, _) in ex.end_points:
+        ax1.plot(x, y, "go")
+
     fig.colorbar(cmap, ax=ax1)
 
     ax2.tricontourf(x2, y2, z2, cmap="RdBu_r")
@@ -44,6 +51,8 @@ def main():
     log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     log_file_name = "extractor.log"
     logging.basicConfig(filename=log_file_name, level=logging.DEBUG, format=log_format)
+    logging.getLogger("matplotlib").setLevel(logging.WARNING)
+    logging.getLogger("PIL").setLevel(logging.WARNING)
 
     angle = float(sys.argv[1])
     step_x = float(sys.argv[2])
