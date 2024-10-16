d47db0874954be3e6a78a65b63580baac7f126fa
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-10-16 17:11:36 +0200

    Prepare more code for GUI.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
 
-
 # Python imports
 import logging
-import sys
-import time
 
 # Local imports
-import ice_extractor as ie
-import ice_plotter as ip
+import ice_gui as ig
 
 logger = logging.getLogger(__name__)
 
 
 def main():
-    start = time.time()
+    # start = time.time()
 
     log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     log_file_name = "extractor.log"
@@ -21,20 +17,21 @@ def main():
     logging.getLogger("matplotlib").setLevel(logging.WARNING)
     logging.getLogger("PIL").setLevel(logging.WARNING)
 
-    filename = (sys.argv[1])
+    gui = ig.IceGUI()
+    gui.run()
 
-    ex = ie.IceExtractor()
-    ex.read_file(filename)
+    # filename = (sys.argv[1])
 
-    plotter = ip.IcePlotter()
-    plotter.get_data_from_extractor(ex)
-    plotter.plot_both()
-    plotter.save_figure("plot.png")
+    # ex.read_file(filename)
 
-    end = time.time()
-    duration = end - start
+    # plotter.get_data_from_extractor(ex)
+    # plotter.plot_both()
+    # plotter.save_figure("plot.png")
 
-    logger.info(f"Duration in seconds: {duration}")
+    # end = time.time()
+    # duration = end - start
+
+    # logger.info(f"Duration in seconds: {duration}")
 
 
 if __name__ == "__main__":
